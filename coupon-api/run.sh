75dbd696beb8554d22ebeffc08c2a9393a2c8608
yogaukha <yoga@natasolusi.co.id>
2017-11-13 20:42:32 +0700

    adding bash file

---
+++ coupon-api/run.sh
@@ -0,0 +1,13 @@
+cp .env.example .env
+#copy installer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+#match the hash
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+#run composer setup
+sudo php composer-setup.php
+#delete uninstaller
+php -r "unlink('composer-setup.php');"
+#run composer install
+php composer.phar install
+#run service host on specific port, for coupon use port 3134
+php -S localhost:3134 -t public
